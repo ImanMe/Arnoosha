5eee069c85ff6e6bd55e4e571ea4d0159fe54866
iman memarpour <iman.memarpour@gmail.com>
2020-08-31 23:38:34 -0400

    spinner completed

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagingComponent } from './components/paging/paging.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagingComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagingComponent
+    PagingComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
