f6e8d80ce310b5d4b458f999ecc85fbe7e8cb0b3
iman memarpour <iman.memarpour@gmail.com>
2020-09-01 06:04:01 -0400

    basket - initial

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'No Found' } },
   { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
